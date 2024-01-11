244aa122b8d2caea9e3041689f2525d4638d4869
Jimmy Mutuku <jimmymutuku1@gmail.com>
2024-01-12 00:50:28 +0300

    mypy

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""mypy-validated changes"""
+from typing import List, Tuple
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """
+    Zooms an array by repeating each element a specified number of times.
+    """
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = (2, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
