d7bc8d3d5a5583949be66f21c45b67961a174e73
Jimmy Mutuku <jimmymutuku1@gmail.com>
2024-01-11 23:39:36 +0300

    define variables

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Math floor"""
+
+
+from math import floor
+
+
+def floor_function(n: float) -> int:
+    """
+    Return the floor of the input float.
+    """
+    return floor(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Float to String"""
+
+
+def to_str(n: float) -> str:
+    """
+    Convert the input float to its string representation.
+    """
+    return str(n)
